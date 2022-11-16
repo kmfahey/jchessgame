a8a4ae2fb26ca8a9496b46af5f756398b2315309
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2022-11-15 20:26:04 -0800

    completed ImageManager class
    class loads all pngs in the images dir, assorts them by filename into a 2d hashmap, and makes each one available via an accessor method
    
    ImageManager accessors take a Dimension argument and use Image.getScaledInstance to return a scaled version of the image (as Graphics.drawImage requires)

--- ImageManager.java
+++ ImageManager.java
@@ -1,65 +1,111 @@
 package com.kmfahey.jchessgame;
 
+import java.awt.Dimension;
 import java.awt.image.BufferedImage;
+import java.awt.Image;
 import java.io.File;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
 import java.util.HashMap;
-import java.util.Set;
-import java.util.Map.Entry;
 import javax.imageio.ImageIO;
 import java.io.IOException;
 import java.io.FileNotFoundException;
-import javax.imageio.stream.ImageInputStream;
+import net.sf.repr.Repr;
 
 
 public class ImageManager {
 
-    HashMap<String,HashMap<String,BufferedImage>> piecesImages = new HashMap<>();
+    private HashMap<String, HashMap<String, Image>> piecesImages;
 
-    public ImageManager(String imageDirectory) throws IOException, FileNotFoundException {
-        
+    public ImageManager(final String imageDirectory) throws IOException, FileNotFoundException {
+
+        piecesImages = new HashMap<>();
+
         Path imageDirPath = Paths.get(imageDirectory);
         try (DirectoryStream<Path> imageDirStream = Files.newDirectoryStream(imageDirPath, "*.png")) {
-            piecesImages.put("chessboard", new HashMap<String,BufferedImage>());
-            piecesImages.put("black", new HashMap<String,BufferedImage>());
-            piecesImages.put("white", new HashMap<String,BufferedImage>());
+            piecesImages.put("chessboard", new HashMap<String, Image>());
+            piecesImages.put("black", new HashMap<String, Image>());
+            piecesImages.put("white", new HashMap<String, Image>());
             for (Path imagePath : imageDirStream) {
                 File imageFile = new File(imagePath.toString());
-                BufferedImage imageData = ImageIO.read(imageFile);
+                Image imageData = ImageIO.read(imageFile);
                 String fileName = imagePath.getFileName().toString();
                 String[] fileNamePieces = fileName.split("[ .-]");
-                if (fileNamePieces[0] == "Chessboard") {
-                    piecesImages.put("chessboard", new HashMap<String,BufferedImage>());
+                if (fileNamePieces[0].equals("Chessboard")) {
+                    piecesImages.put("chessboard", new HashMap<String, Image>());
                     piecesImages.get("chessboard").put("chessboard", imageData);
                     continue;
                 }
                 String pieceColor = fileNamePieces[0].toLowerCase();
                 String pieceRole = fileNamePieces[1].toLowerCase();
-                String handedness = "";
                 if (pieceRole.equals("knight")) {
-                    handedness = fileNamePieces[2].toLowerCase();
+                    String handedness = fileNamePieces[2].toLowerCase();
                     pieceRole = handedness + pieceRole;
                 }
                 piecesImages.get(pieceColor).put(pieceRole, imageData);
             }
-            for (Entry<String,HashMap<String,BufferedImage>> hashCatgEntry : piecesImages.entrySet()) {
-                String catKey = hashCatgEntry.getKey();
-                for (Entry<String,BufferedImage> imgHashEntry : hashCatgEntry.getValue().entrySet()) {
-                    String imgKey = imgHashEntry.getKey();
-                    if (catKey == "chessboard") {
-                        System.out.println(catKey);
-                    } else {
-                        System.out.println(catKey + " " + imgKey);
-                    }
-                }
-            }
         }
     }
 
-    public static void main(String[] args) throws IOException, FileNotFoundException {
-        ImageManager imgMgr = new ImageManager("/home/kmfahey/Workspace/jchessgame/images/");
+    public Image getChessboard(final Dimension imageDims) {
+        return piecesImages.get("chessboard").get("chessboard").getScaledInstance((int) imageDims.getWidth(), (int) imageDims.getHeight(), Image.SCALE_FAST);
+    }
+
+    public Image getBlackBishop(final Dimension imageDims) {
+        return piecesImages.get("black").get("bishop").getScaledInstance((int) imageDims.getWidth(), (int) imageDims.getHeight(), Image.SCALE_FAST);
+    }
+
+    public Image getBlackKing(final Dimension imageDims) {
+        return piecesImages.get("black").get("king").getScaledInstance((int) imageDims.getWidth(), (int) imageDims.getHeight(), Image.SCALE_FAST);
+    }
+
+    public Image getBlackKnightLeft(final Dimension imageDims) {
+        return piecesImages.get("black").get("knightleft").getScaledInstance((int) imageDims.getWidth(), (int) imageDims.getHeight(), Image.SCALE_FAST);
+    }
+
+    public Image getBlackKnightRight(final Dimension imageDims) {
+        return piecesImages.get("black").get("knightright").getScaledInstance((int) imageDims.getWidth(), (int) imageDims.getHeight(), Image.SCALE_FAST);
+    }
+
+    public Image getBlackPawn(final Dimension imageDims) {
+        return piecesImages.get("black").get("pawn").getScaledInstance((int) imageDims.getWidth(), (int) imageDims.getHeight(), Image.SCALE_FAST);
+    }
+
+    public Image getBlackQueen(final Dimension imageDims) {
+        return piecesImages.get("black").get("queen").getScaledInstance((int) imageDims.getWidth(), (int) imageDims.getHeight(), Image.SCALE_FAST);
+    }
+
+    public Image getBlackRook(final Dimension imageDims) {
+        return piecesImages.get("black").get("rook").getScaledInstance((int) imageDims.getWidth(), (int) imageDims.getHeight(), Image.SCALE_FAST);
+    }
+
+    public Image getWhiteBishop(final Dimension imageDims) {
+        return piecesImages.get("white").get("bishop").getScaledInstance((int) imageDims.getWidth(), (int) imageDims.getHeight(), Image.SCALE_FAST);
+    }
+
+    public Image getWhiteKing(final Dimension imageDims) {
+        return piecesImages.get("white").get("king").getScaledInstance((int) imageDims.getWidth(), (int) imageDims.getHeight(), Image.SCALE_FAST);
+    }
+
+    public Image getWhiteKnightLeft(final Dimension imageDims) {
+        return piecesImages.get("white").get("knightleft").getScaledInstance((int) imageDims.getWidth(), (int) imageDims.getHeight(), Image.SCALE_FAST);
+    }
+
+    public Image getWhiteKnightRight(final Dimension imageDims) {
+        return piecesImages.get("white").get("knightright").getScaledInstance((int) imageDims.getWidth(), (int) imageDims.getHeight(), Image.SCALE_FAST);
+    }
+
+    public Image getWhitePawn(final Dimension imageDims) {
+        return piecesImages.get("white").get("pawn").getScaledInstance((int) imageDims.getWidth(), (int) imageDims.getHeight(), Image.SCALE_FAST);
+    }
+
+    public Image getWhiteQueen(final Dimension imageDims) {
+        return piecesImages.get("white").get("queen").getScaledInstance((int) imageDims.getWidth(), (int) imageDims.getHeight(), Image.SCALE_FAST);
+    }
+
+    public Image getWhiteRook(final Dimension imageDims) {
+        return piecesImages.get("white").get("rook").getScaledInstance((int) imageDims.getWidth(), (int) imageDims.getHeight(), Image.SCALE_FAST);
     }
 }
